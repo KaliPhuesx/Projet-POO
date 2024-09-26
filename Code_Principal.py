6ff1cfdf0c92a79e66c268a3f8d11fc4ee5efe26
Loupson <chatlouison@gmail.com>
2024-09-26 11:34:30 +0200

    Update Code_Principal.py

--- Code_Principal.py
+++ Code_Principal.py
@@ -1,7 +1,16 @@
 from Personnage import *
+from Epee import *
 
 p1=Personnage("Conan",78)
 p2=Personnage("Lannister",45)
+epee1=Epee("Excalibur", 7)
+epee2=Epee("Durandal", 4)
+
+liste_epee=[]
+liste_epee.append(epee1)
+liste_epee.append(epee2)
+for j in liste_epee:
+    print(j)
 
 liste_perso=[]
 

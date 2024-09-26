ed71c384625d2c9494a43946ca36d5ab9367d519
Loupson <chatlouison@gmail.com>
2024-09-26 11:41:43 +0200

    Update Epee_class.py

--- Epee_class.py
+++ Epee_class.py
@@ -1,4 +1,3 @@
-# Créé par Eleve, le 26/09/2024 en Python 3.7
 class Epee:
     __nom=""
     __niveauAttaque=0
@@ -15,5 +14,5 @@ class Epee:
         return self.__niveauAttaque
 
     def __str__(self):
-        total=self.__nom+"  "+str(self.__niveauAttaque)
-        return total
+        self.__nom+"  "+str(self.__niveauAttaque)
+        return ("L'épée se nomme : "+self.__nom+" et fait "++str(self.__niveauAttaque)+" points de dégâts.")

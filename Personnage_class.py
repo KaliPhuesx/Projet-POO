a144310ce5d2ddc27931f0f2221f108aedfbba7c
Loupson <chatlouison@gmail.com>
2024-10-10 10:09:42 +0200

    Update Personnage_class.py

--- Personnage_class.py
+++ Personnage_class.py
@@ -11,5 +11,21 @@ class Personnage:
     def getHp(self):
         return self.__hp
     
-    def __str__(self):
-        return (" Name : " +self.__name+" and have : "+self.__hp+ "HP")
+    d def __str__(self):
+        if self.__armeEnMain==None:
+            return ("Le personnage s'appelle "+self.__nom+", il a "+str(self.__hp)+" points de vie")
+        else:
+            return ("Le personnage s'appelle "+self.__nom+", il a "+str(self.__hp)+" points de vie, et porte actuellement sur lui : "+str(self.__armeEnMain))
+
+    def choisirArmeEnMain(self,EPEE):
+        self.__armeEnMain=EPEE
+
+    def getarmeenmain(self):
+        return self.__armeEnMain
+
+    def AjoutARMEinventaire(self,liste:list,epee):
+        if len(liste)<5:
+            liste.append(epee)
+        return liste
+
+    
